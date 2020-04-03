562b16793566237c714b6ab9adba9431f0409ae7
Sebastian Grzelak <segrz8@gmail.com>
2020-04-03 13:29:08 +0200

    Updates

---
+++ precache-manifest.908594529131e7355aea4f548927334e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20be3bfc0a9a5c31db4ec739763c6822",
+    "url": "/SmartControl/index.html"
+  },
+  {
+    "revision": "b7176f08b36e69d22b75",
+    "url": "/SmartControl/static/css/main.634c7840.chunk.css"
+  },
+  {
+    "revision": "a2dd8870ba7b67ecfd5c",
+    "url": "/SmartControl/static/js/2.6a17001f.chunk.js"
+  },
+  {
+    "revision": "cb6e7e2a94b35652a337effd4fbe5922",
+    "url": "/SmartControl/static/js/2.6a17001f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7176f08b36e69d22b75",
+    "url": "/SmartControl/static/js/main.72fabaf7.chunk.js"
+  },
+  {
+    "revision": "8c24481db73bcb864aef",
+    "url": "/SmartControl/static/js/runtime-main.20de275e.js"
+  },
+  {
+    "revision": "dd43e9fb53fd028027a20018c340c66a",
+    "url": "/SmartControl/static/media/busB.dd43e9fb.jpg"
+  },
+  {
+    "revision": "bf38f0788a3d39f3eec898ed157a064b",
+    "url": "/SmartControl/static/media/logoSC.bf38f078.jpg"
+  },
+  {
+    "revision": "f44f9cde71a57b434eed18019833b73f",
+    "url": "/SmartControl/static/media/logoSCs.f44f9cde.jpg"
+  }
+]);
